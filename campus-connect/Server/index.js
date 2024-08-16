145a741e4c3eb39d80b12b6d05a2ab0a87eeafbe
brutalkiller007 <janakiram123m@gmail.com>
2024-08-17 02:53:35 +0530

    account creation done

--- campus-connect/Server/index.js
+++ campus-connect/Server/index.js
@@ -2,13 +2,20 @@ const express = require("express");
 const app = express();
 
 require("dotenv").config();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 
 app.listen(PORT, () => {
     console.log("SERVER SUCCESSFULL");
 })
+
+const db_connect = require("./Config/database");
+db_connect();
+
+const user_routes = require("./Routes/User");
+
+app.use("/api/v1/auth", user_routes);
 
 app.get("/", (req, res) => {
     res.json({
