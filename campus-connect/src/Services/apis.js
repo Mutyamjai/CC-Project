40d7abfbb5227b6de5aa99f59fda55382d97b62e
brutalkiller007 <janakiram123m@gmail.com>
2024-08-17 19:00:52 +0530

    login

--- campus-connect/src/Services/apis.js
+++ campus-connect/src/Services/apis.js
@@ -3,4 +3,5 @@ const BASE_URL = process.env.REACT_APP_BASE_URL
 export const auth = {
     SEND_OTP : BASE_URL + '/auth/send_otp',
     SIGN_UP : BASE_URL + '/auth/sign_up',
+    LOG_IN : BASE_URL + '/auth/login'
 }
