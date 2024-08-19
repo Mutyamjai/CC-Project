0f2e1743b80f0b4182575d548cc55673a410bc8b
brutalkiller007 <janakiram123m@gmail.com>
2024-08-20 02:01:24 +0530

    all server laundry done

--- campus-connect/Server/Routes/Laundry.js
+++ campus-connect/Server/Routes/Laundry.js
@@ -3,12 +3,13 @@ const router = express.Router();
 
 const { auth, is_laundry, is_student } = require("../Middlewares/Auth");
 const {create_order} = require("../Controllers/Laundry/CreateOrder");
-const {fetch_order_number} = require("../Controllers/Laundry/FetchOrderNumber");
+const {fetch_order_number, fetch_order_details} = require("../Controllers/Laundry/FetchOrderNumber");
 const {fetch_under_washing_orders, fetch_ready_to_collect_orders, fetch_completed_orders, fetch_student_active_orders, fetch_student_completed_orders} = require("../Controllers/Laundry/FechingOrders");
 const {make_ready_to_collect, make_it_completed_order, paid_in_cash, paid_in_online} = require("../Controllers/Laundry/UpdateStatus")
 
 router.post("/create_order",auth, is_laundry, create_order);
 router.get("/fetch_order_number",auth, is_laundry, fetch_order_number);
+router.post("/fetch_order_details",auth , fetch_order_details);
 
 router.post("/fetch_under_washing_orders",auth, is_laundry, fetch_under_washing_orders);
 router.post("/fetch_ready_to_collect_orders",auth, fetch_ready_to_collect_orders);
