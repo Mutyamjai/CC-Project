9e7df30db955e23f7cd8a4bda9e149f12daa9755
Mutyamjai <22ucs130@lnmiit.ac.in>
2024-11-21 05:38:47 +0530

    Update index.js

--- campus-connect/Server/index.js
+++ campus-connect/Server/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 4000;
 app.use(express.json());
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: "https://campusconnectfrontend.vercel.app",
         credentials: true,
     })
 )
